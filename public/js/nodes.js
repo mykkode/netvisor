be983eb51dee179cb7100d09c197f2014d9ae56e
Trifu Marius-Cosntantin <trifumarius01@gmail.com>
2018-10-28 08:18:42 +0200

    nodes complete

--- public/js/nodes.js
+++ public/js/nodes.js
@@ -77,15 +77,3 @@ $(document).ready(function () {
     }
     makeCode();
 });
-
-
-var qrcode = new QRCode(document.getElementById("qrcode"), {
-    width : 100,
-    height : 100
-});
-function makeCode () {
-    var elText = document.getElementById("text");
-
-    qrcode.makeCode("fdfsdf");
-}
-makeCode();
